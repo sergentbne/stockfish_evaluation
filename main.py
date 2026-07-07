cea7dcd43b4b326eddc3bd2ab68e90e3b3455f0f
Louis Turpault <louis.turpault@gmail.com>
2026-07-07 12:46:51 -0400

    feat: higher performance

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import stockfish
 import logging
 import multiprocessing
 from commons import BINARY_PATH
-from stockfish_handler import ensure_stockfish_binary
+from stockfish_downloader import ensure_stockfish_binary
 
 
 log = logging.Logger(__name__)
@@ -15,7 +15,7 @@ def main():
     # New Stockfish instance starts at default starting position.
     # No FEN/position set — queries analyze the standard chess startpos.
     fish1 = stockfish.Stockfish(str(BINARY_PATH), depth=1)
-    fish2 = stockfish.Stockfish(str(BINARY_PATH), depth=25)
+    fish2 = stockfish.Stockfish(str(BINARY_PATH), depth=2)
 
     _ = main_loop(fish1, fish2)
 
@@ -30,7 +30,7 @@ def main_loop(stockfish1: stockfish.Stockfish, stockfish2: stockfish.Stockfish) 
     moves: list[str] = []
     queue_of_uci: multiprocessing.Queue[str | None] = multiprocessing.Queue()
 
-    process = multiprocessing.Process(target=display_moves, args=(queue_of_uci,))
+    process = multiprocessing.Process(target=display_moves_start, args=(queue_of_uci,))
     process.start()
     while not is_mate:
         if move := stockfish1.get_best_move():
@@ -58,35 +58,33 @@ def main_loop(stockfish1: stockfish.Stockfish, stockfish2: stockfish.Stockfish) 
     return has_fish1_won
 
 
-def display_moves(queue_of_uci: multiprocessing.Queue[str]):
+def display_moves_start(queue_of_uci: multiprocessing.Queue[str]):
     import chess
     import chess.svg
-
-    import imgcat
 
     board = chess.Board()
     while move := queue_of_uci.get(block=True):
         _ = board.push_uci(move)
         svg_string = chess.svg.board(board, size=1200)
-        image_buffer = svg_to_png(svg_string)
-        imgcat.imgcat(image_buffer.getvalue())
-
+        image_bytes = svg_to_png(svg_string)
+        display_image_to_screen(image_bytes)
     return
 
 
 def svg_to_png(svg_string: str):
 
-    from svglib.svglib import svg2rlg
-    from reportlab.graphics import renderPM
-    import io
+    import cairosvg
 
-    svg_buffer = io.BytesIO(svg_string.encode("utf-8"))
-    drawing_of_svg = svg2rlg(svg_buffer)
-    if drawing_of_svg is None:
-        raise Exception("unreachable")
-    image_buffer = io.BytesIO()
-    _ = renderPM.drawToFile(drawing_of_svg, image_buffer, fmt="png")
-    return image_buffer
+    image_bytes = cairosvg.svg2png(bytestring=svg_string)
+    assert isinstance(image_bytes, bytes)
+    return image_bytes
+
+
+def display_image_to_screen(image_bytes: bytes):
+
+    import imgcat
+
+    imgcat.imgcat(image_bytes)
 
 
 def apply_moves_to_all_fishes(
