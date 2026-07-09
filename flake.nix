11cd6cad41ed41b2d2c7d0b4e38797aa0055fdee
Louis Turpault <louis.turpault@gmail.com>
2026-07-09 14:39:27 -0400

    multisystem

--- flake.nix
+++ flake.nix
@@ -7,23 +7,36 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      each_system = f: nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        f {
+          inherit pkgs system;
+        }
+      );
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.cairo
-          pkgs.libxcb
-		  pkgs.gcc
-          pkgs.pkg-config
-		  pkgs.uv
-        ];
-        shellHook = ''
-          export PKG_CONFIG_PATH="${pkgs.cairo.dev}/lib/pkgconfig:${pkgs.libxcb.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-          export DYLD_FALLBACK_LIBRARY_PATH="${pkgs.cairo}/lib:$DYLD_FALLBACK_LIBRARY_PATH"
-          export LD_LIBRARY_PATH="${pkgs.cairo}/lib:$LD_LIBRARY_PATH"
-        '';
-      };
+      devShells = each_system ({ pkgs, system }: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.cairo
+            pkgs.libxcb
+            pkgs.gcc
+            pkgs.pkg-config
+            pkgs.uv
+          ];
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.cairo.dev}/lib/pkgconfig:${pkgs.libxcb.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            export DYLD_FALLBACK_LIBRARY_PATH="${pkgs.cairo}/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+            export LD_LIBRARY_PATH="${pkgs.cairo}/lib:$LD_LIBRARY_PATH"
+          '';
+        };
+      });
     };
 }
